d51faadabac6dd5f3fd605a0e47911fea05e7558
Madison <mahamilton@mills.edu>
2017-02-14 21:50:22 +0000

    second

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//var template = require('skel.min');
 
 var index = require('./app_server/routes/home');
 var users = require('./app_server/routes/users');
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -6,5 +6,5 @@
 //and JS data obj
 module.exports.home = function(req, res){
     //console.log(req.method, req.url);
-    res.render('layout', { title: 'Day Planner' });
+    res.render('index', { title: 'Day Planner' });
 };
