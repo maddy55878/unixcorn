463c6591c2eb3c06af1dddd55eb262da570b610e
Angelica Leyva-McMurtry <aleyvamcmurtry@mills.edu>
2017-03-14 05:43:07 +0000

    Clean Up 1

--- app_server/controllers/activityControl.js
+++ app_server/controllers/activityControl.js
@@ -1,24 +1,11 @@
 var mongoose = require('mongoose');
 
-// module.exports.planner = function(req, res){
-//     //console.log(req.method, req.url);
-//     res.render('act1', { title: 'act2' });
-// };
-
-
 var activityResults = mongoose.model('activityResults');
-//var activityResults2 = mongoose.model('activityResults2');
 var sendJSONresponse = function(res, status, content){
     res.status(status);
     res.json(content);
 };
 
-
-// module.exports.find2 = function(req, res){
-//     activityResults2.findOne({name:req.params.name}).exec(function(err,data){
-//         sendJSONresponse(res,200, data);
-//     });
-// };
 
 
 module.exports.find = function(req, res){
@@ -40,19 +27,8 @@ module.exports.getAll = function(req, res) {
 
   activityResults
     .find()    //exec tells to execute query 
-    .exec(function(err, data) {    //faculty is the return data
-      //console.log("Found Faculty", faculty.length);
-      //console.log(faculty);
+    .exec(function(err, data) { 
 sendJSONresponse(res, 200, data);
     });
 
 };
-
-//doesn't do anything
-module.exports.getCollections = function(req, res){
-    activityResults
-        .getCollectionNames().exec(function(err, data){
-            sendJSONresponse(res, 200, data);
-        });
-};
-
--- app_server/routes/home.js
+++ app_server/routes/home.js
@@ -5,19 +5,10 @@ var ctrlActivity = require('../controllers/activityControl');
 
 router.get('/', ctrlMain.layout);
 router.get('/planner', ctrlMain.planner);
-//router.get('/act2', ctrlActivity.layout);
-//router.get('/activityResults', ctrlActivity.layout);
-//router.get('/activityResults/:price', ctrlActivity.find);
-//router.get('/activityResults/:price', ctrlActivity.find);
 router.get('/activityResults/:name', ctrlActivity.find);
-//router.get('/activityResults/:name2', ctrlActivity.find2);
 router.get('/findbyid/:activityId', ctrlActivity.findId);
 router.get('/getAll', ctrlActivity.getAll);
-router.get('/getCollections', ctrlActivity.getCollections); 
 router.get('/about', ctrlMain.about);
 router.get('/contact', ctrlMain.contact);
-
-//testing route 
-//router.get('/testing', ctrlMain.testing);
 
 module.exports = router;
