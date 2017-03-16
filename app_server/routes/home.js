859b73b470bf1c995cd6219c75f2b9692c766da2
Angelica Leyva-McMurtry <aleyvamcmurtry@mills.edu>
2017-03-16 05:04:38 +0000

    new info in databases

--- app_server/routes/home.js
+++ app_server/routes/home.js
@@ -5,19 +5,21 @@ var ctrlActivity = require('../controllers/activityControl');
 
 router.get('/', ctrlMain.layout);
 router.get('/planner', ctrlMain.planner);
-router.get('/activityResults/:name', ctrlActivity.findResults);
-router.get('/findbyidResults/:activityId', ctrlActivity.findIdResults);
-router.get('/getAllResults', ctrlActivity.getAllResults);
-router.get('/activityResults2/:name', ctrlActivity.findResults2);
-router.get('/findbyidResults2/:activityId2', ctrlActivity.findIdResults2);
-router.get('/getAllResults', ctrlActivity.getAllResults2);
-router.get('/userInputs/:name', ctrlActivity.findUserInputs);
-router.get('/findbyid/:userInputsId', ctrlActivity.findIdUserInputs);
-router.get('/getAllUserInputs', ctrlActivity.getAllUserInputs);
 router.get('/about', ctrlMain.about);
 router.get('/contact', ctrlMain.contact);
 router.get('/events', ctrlMain.events);
 router.get('/events/login',  ctrlMain.login);
 router.get('/events/signup', ctrlMain.signup);
+
+//Schemas
+router.get('/activityResults/:name', ctrlActivity.findResults);
+router.get('/findbyidResults/:activityId', ctrlActivity.findIdResults);
+router.get('/getAllResults', ctrlActivity.getAllResults);
+router.get('/activityResults2/:name', ctrlActivity.findResults2);
+router.get('/findbyidResults2/:activityId2', ctrlActivity.findIdResults2);
+router.get('/getAllResults2', ctrlActivity.getAllResults2);
+router.get('/userInputs/:username', ctrlActivity.findUserInputs);
+router.get('/findbyid/:userInputsId', ctrlActivity.findIdUserInputs);
+router.get('/getAllUserInputs2', ctrlActivity.getAllUserInputs);
 
 module.exports = router;
